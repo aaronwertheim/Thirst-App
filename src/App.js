710e785340e00e126459dbdde192c6fa9aecc088
aaron wertheim <aawertheim@gmail.com>
2022-10-06 16:30:07 -0500

    bg-image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Nav from "./Nav";
 function App() {
 
   return (
-    <div className="App">
+    <div class="bg-[url('https://static01.nyt.com/images/2013/12/08/magazine/08wmt/mag-08WMT-t_CA0-superJumbo.jpg')] h-screen overflow-auto bg-no-repeat bg-cover">
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
