f6939349be1d52422afa9a530619822dffcae099
aaron wertheim <aawertheim@gmail.com>
2022-10-10 15:32:51 -0500

    icon, deleted extra files

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,8 @@ function Nav() {
         <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light">
             <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">  
                 <div class="collapse navbar-collapse flex-grow items-center">
-                    <div class="text-xl text-white pr-2 font-semibold">Drink App</div>   
+                
+                    <div class="text-xl text-white pr-2 font-semibold"><i class="fas fa-glass-martini-alt"></i></div>   
                 </div>
                 <ul class="navbar-nav flex  pl-0 list-style-none mr-auto">
                     <li class="nav-item p-2">
